51ab9c546e665d1371cf77f98e7c242ac0571f21
xiaogeVIP919 <xiaogeVIP919@163.com>
2021-03-28 15:54:04 +0800

    1

--- src/com/huawei/ReturnTheNumberOf1InBinary.java
+++ src/com/huawei/ReturnTheNumberOf1InBinary.java
@@ -1,4 +1,18 @@
 package com.huawei;
 
+import java.util.Scanner;
+
 public class ReturnTheNumberOf1InBinary {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int n = sc.nextInt();
+            int count = 0;
+            while (n != 0){
+                count++;
+                n = n & n - 1;
+            }
+            System.out.println(count);
+        }
+    }
 }
